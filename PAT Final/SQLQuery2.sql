7ce0b956736f8c7fd9d62ce0a3919c9701b58a5d
skilye <47102389+skilye@users.noreply.github.com>
2019-11-07 14:35:20 +1100

    Add files via upload
    fixed some UI problems with the overall website. added how to page. took away 404 page

---
+++ PAT Final/SQLQuery2.sql
@@ -0,0 +1,27 @@
+select * FROM Major
+
+INSERT INTO Major (
+	MajorID,
+	MajorName
+)
+
+VALUES
+	('BUSTECH', 'Business Technology Major'),
+	('SYSDEV', 'Systems Devlopment Major'),
+	('INTMED', 'Interactive Media Major'),
+	('NURS', 'Nursing') ;
+
+Select * FROM Administrator
+
+INSERT INTO Administrator(
+	adminID,
+	fName,
+	mName,
+	lName,
+	clearanceLevel
+)
+Values
+	('001', 'Lachlan', 'Geoffrey', 'Barden', '1'),
+	('002', 'Kim', 'Anne', 'Hempsall', '1'),
+	('003', 'Bohan', 'John', 'He', '1'),
+	('004', 'Shenchong', 'Paul', 'Chen', '1') ;
